44a69cc19d51823cacdea19f2c5a357832d86d4f
jedychen <jedy829@gmail.com>
2020-03-16 23:40:07 +0800

    Change Dragging to Use Interact JS
    Remove Draggabilly since it doesn’t support transform:rotation.
    
    Summary:
    -Enable dragging function.
    -Add bounce-back effect after an element being dragged.
    -Remove Bouncing effects after they finish.
    -Remove extra html structure.

--- src/assets/js/cover_card.js
+++ src/assets/js/cover_card.js
@@ -1,5 +1,5 @@
-import Bounce from 'bounce.js';
-import Draggabilly from 'draggabilly'
+import Bounce from 'bounce.js'
+import Interact from 'interactjs'
 import { cross } from 'mathjs'
 
 
@@ -21,56 +21,70 @@ export function enterAnimation() {
       duration: 2000,
     })
   // bounce.define("my-animation");
-  bounce.applyTo(document.querySelectorAll(".main-card-container__bounce"))
-  move.applyTo(document.querySelectorAll(".main-card-list"))
+  bounce.applyTo(document.querySelectorAll(".main-card-container"), {remove: true})
+  move.applyTo(document.querySelectorAll(".main-card-list"), {remove: true})
 }
 
 export function dragCards() {
-  // get all draggie elements
-  let draggableElems = document.querySelectorAll('.draggable');
-  // array of Draggabillies
-  let draggies = []
-  // init Draggabillies
-  for (var draggableElem of draggableElems) {
-    var draggie = new Draggabilly( draggableElem, {
-      // options...
-    })
-    draggie.origin = {x:0, y:0}
-    draggie.rotation = 0
-    draggie.id = draggableElem.id
-    draggie.on( 'dragStart', function() {
-      const element = document.querySelector('#'+ this.id).getBoundingClientRect()
-      this.origin.x = element.left
-      this.origin.y = element.top
-    })
-    draggie.on( 'dragMove', function(event, pointer, moveVector) {
-      let element = document.querySelector('#'+ this.id)
-      const elementRect = element.getBoundingClientRect()
-      let id = document.querySelector('#info');
-      const elementCenter = {
-        x: elementRect.left + elementRect.width * 0.5,
-        y: elementRect.top + elementRect.height * 0.5,
+    let draggie = Interact('.draggable')
+    draggie.transform = { x: 0, y: 0, rotation: 0}
+    draggie.originPos = { x: 0, y: 0}
+    draggie
+    .draggable({ 
+      autoScroll: true,
+      listeners: {
+        start (event) {
+        },
+        move: dragMoveListener,
+        end (event) {
+          var target = event.target
+          target.style.transform = 'none'
+          const transformedX = (parseFloat(target.getAttribute('data-x')) || 0)
+          const transformedY = (parseFloat(target.getAttribute('data-y')) || 0)
+          let move = new Bounce();
+          move
+            .translate({
+              from: {x: transformedX, y: transformedY},
+              to: {x: 0, y: 0},
+              duration: 1000,
+            })
+          move.applyTo(event.target, {remove: true})
+          target.setAttribute('data-x', 0)
+          target.setAttribute('data-y', 0)
+        }
       }
-      const pointerToCenter = [
-        pointer.pageX - elementCenter.x,
-        pointer.pageY - elementCenter.y,
-        0,
-      ]
-      const moveDirection = [moveVector.x, moveVector.y, 0]
-      //sign of z in the cross product of two vecoters
-      // indicates if rotation is clockwise
-      var rotation = cross(pointerToCenter, moveDirection)
-      var rotationComputed = rotation[2]/1000
-      id.innerHTML = 'Rotation: ' + rotationComputed + '<br>Name: ' + element.id
-      let elementNew = document.querySelector('#main-card-work')
-      this.rotation = rotationComputed
-      elementNew.style.transform = 'rotate(90deg)';
-      // elementNew.style = '{ transform: translate3D(10px, 5px, 0) rotate(20deg) }'
     })
-    draggies.push( draggie )
-  }
 
-  return draggies
+  return draggie
+}
+
+function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+  // add rotation based on dragging force
+  const elementRect = target.getBoundingClientRect()
+  const elementCenter = {
+    x: elementRect.left + elementRect.width * 0.5,
+    y: elementRect.top,
+  }
+  const pointerToCenter = [
+    event.x0 - elementCenter.x,
+    event.y0 - elementCenter.y,
+    0,
+  ]
+  const moveDirection = [event.dx, event.dy, 0]
+  //sign of z in the cross product of two vecoters
+  // indicates if rotation is clockwise
+  var rotation = cross(pointerToCenter, moveDirection)
+  var rotationComputed = rotation[2]/100
+  let id = document.querySelector('#info');
+  id.innerHTML = 'PageCenter: ' + elementCenter.x + ', ' + elementCenter.y + '<br>InitMousePos: ' + event.x0 + ', ' + event.y0 + '<br>Rotation: ' + rotationComputed
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px) rotate(' + rotationComputed + 'deg)'
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
 }
 
 // export function Line(obj){
